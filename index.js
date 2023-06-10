612d89b7aa29d3b83ce02525b45778b6e5351a13
Bakhita87 <bakhitaotieno@gmail.com>
2023-06-10 20:35:28 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string){
+    return string.toLowerCase();
+  }
+
+  function logShout(string){
+    console.log(string.toUpperCase())
+  }
+
+  function logWhisper(string){
+    console.log(string.toLowerCase())
+  }
+
+  function sayHiToHeadphonedRoommate(string){
+    return  "YES INDEED!".toUpperCase()
+  }
+
+  
+function sayHiToHeadphonedRoommate(){
+    const say = "Let's have dinner together!";
+    return 'YES INDEED!'
+}
+
+function  sayHiToHeadphonedRoommate(){
+    return "I can't hear you!"
+}
+
+function sayHiToHeadphonedRoommate(){
+    const say = "Let's have dinner together!";
+    return "I would love to!"
+}
